a90119e440dc3483d46e24fc7910c8e3ca1e5fb0
Jeremiah Akpera <jerryakpera@gmail.com>
2024-03-26 08:11:29 +0100

    asgi/wsgi settings import fix

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from config.settings import base
+
+if base.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_asgi_application()
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from config.settings import base
+
+if base.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_wsgi_application()
